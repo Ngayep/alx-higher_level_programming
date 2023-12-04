b75e8b5d55e2f38d2a4013598665edeaad8c78fa
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-04 00:09:51 -0800

    multiple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,10 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
 def multiple_returns(sentence):
 
-    lenght = len(sentence)
-    first_char = sentence[0] if lenght > 0 else "None"
-    new_tuple = lenght, first_char
+    new_tuple = ()
+    if len(sentence) == 0:
+        new_tuple = 0, None
+    else:
+        new_tuple = len(sentence), sentence[0]
     return new_tuple
