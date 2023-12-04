629835df85d3675b3379d259df5f1ba6c46bc39b
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-04 00:33:58 -0800

    max value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+def max_integer(my_list=[]):
+
+    if len(my_list) == 0:
+        return None
+
+    Max_value = my_list[0]
+
+    for i in range(1, len(my_list)):
+        if my_list[i] > Max_value:
+            Max_value = my_list[i]
+
+    return Max_value
