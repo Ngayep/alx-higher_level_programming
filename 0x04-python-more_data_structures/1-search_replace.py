acbaa8699e198b6c9c9175bfa489bcf9bf59264f
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-06 14:35:08 -0800

    intersection

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,5 @@
 
 def search_replace(my_list, search, replace):
 
-    return (list(map(lambda x: replace if x is search else x, my_list)))
+    new_list = [word if word != search else replace for word in my_list]
+    return new_list
