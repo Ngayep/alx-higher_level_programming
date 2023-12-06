a10890a126a130e6439be4f9a89bb1350c6899da
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-06 14:14:17 -0800

    file

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-Prototype: def search_replace(my_list, search, replace):
+def search_replace(my_list, search, replace):
 
     return (list(map(lambda x: replace if x is search else x, my_list)))
