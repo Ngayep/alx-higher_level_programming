faa94f2e7ab1bef467528ce84b40406239ff2aac
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-06 13:53:33 -0800

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,4 @@
 
 Prototype: def search_replace(my_list, search, replace):
 
-    new_list = [word if word != search else replace for word in my_list]
-
-    return new_list
+    return (list(map(lambda x: replace if x is search else x, my_list)))
