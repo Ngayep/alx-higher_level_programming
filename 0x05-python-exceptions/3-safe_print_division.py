437973405c37fcd3807f469d8e8a09b20acc1b0e
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-18 14:36:44 -0800

    division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
 
     try:
         quotient = a / b
-    except (ZeroDivisionError, TypeError):
+    except (TypeError, ZeroDivisionError):
         quotient = None
     finally:
         print("Inside result: {}".format(quotient))
