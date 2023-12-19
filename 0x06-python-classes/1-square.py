c6c1f01d808ee469c01c78cbc0d310fc06e38da8
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-19 11:30:45 -0800

    square init

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Defines a square with a private instance attribute."""
+
+
+class Square:
+    """private attribute ."""
+
+    def __init__(self, size):
+        """initialize a new Square."""
+
+        self.__size = size
