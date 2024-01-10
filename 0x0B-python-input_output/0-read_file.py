d7ffe030af2e4e2b7d32bb9b139716a8fdda0786
Ngayep <courtesyofjess@gmail.com>
2024-01-10 08:39:35 +0100

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def read_file(filename=""):
     """
     Reads a text file (attempting UTF-8 encoding) and prints it to stdout.
@@ -8,8 +7,10 @@ def read_file(filename=""):
         filename: The name of the file to read.
     """
     try:
-        with open(filename, mode="r", encoding="utf-8") as file:
+        with open(filename, 'r', encoding='utf-8') as file:
             for line in file:
                 print(line, end="")
-    except (IOError, UnicodeDecodeError) as e:
-        print(f"Error reading file: {e}")
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
