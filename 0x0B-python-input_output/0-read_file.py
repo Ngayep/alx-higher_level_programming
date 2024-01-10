126bef21232e6eabb1cc162cf3324b4de20f1fe6
Ngayep <courtesyofjess@gmail.com>
2024-01-10 08:25:44 +0100

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-
 def read_file(filename=""):
-    """reads a text file and prints it to stdout"""
-
-    with open(filename.txt, 'r', encoding="utf-8") as file:
-        print(file.read(), end="")
+    with open(filename, encoding="utf-8") as f:
+        lineNum = 1
+        while(True):
+            line = f.readline()
+            if not line:
+                break
+            print(line, end="")
+            lineNum += 1
