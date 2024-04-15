ffb8455b17ca2a1ae977d2a79777e83cc6692924
Ngayep <courtesyofjess@gmail.com>
2024-04-15 23:55:25 +0100

    base class

---
+++ 0x0A-python-inheritance/6_base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""base class with an exception raised"""
+
+
+class BaseGeometry:
+    """method that raises an exception"""
+
+    def area(self):
+        """method not implemented yet"""
+        raise Exception("area() is not implemented")
