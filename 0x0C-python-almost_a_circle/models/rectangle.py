39543314610fbd5f638b7110dfbc5e6d090f0a86
Ngayep <courtesyofjess@gmail.com>
2024-04-22 15:52:45 +0100

    exceptions added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,11 +23,43 @@ class Rectangle(Base):
             """height getter"""
             return self.__height
 
+        def x(self):
+            """x getter"""
+            return self.__x
+
+        def y(self):
+            """x getter"""
+            return self.__y
+
         @setters
         def width(self, value):
             """ width setter"""
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if width <= 0:
+                raise ValueError("width must be > 0")
             self.__width = value
 
         def height(self, value):
             """height setter"""
-            self.__height = value
+            if not isinstance(value, int):
+                raise TypeError("heigth must be an integer")
+            if heigth <= 0:
+                raise ValueError("heigth must be > 0")
+            self.__width = value
+
+        def x(self, value):
+            """x getter"""
+            if not isinstance(value, int):
+                raise TypeError("x must be an integer")
+            if x < 0:
+                raise ValueError("x must be >= 0")
+            self.__x = value
+
+        def y(self, value):
+            """x getter"""
+            if not isinstance(value, int):
+                raise TypeError("y must be an integer")
+            if y < 0:
+                raise ValueError("y must be >= 0")
+            self.__y = value
