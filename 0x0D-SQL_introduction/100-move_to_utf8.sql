c4b1c4bdb8ff90a374248ee682881fee1d885135
Ngayep <courtesyofjess@gmail.com>
2024-05-21 23:46:07 +0100

    100 done

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,11 +3,6 @@
 -- Convert the database character set and collation
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Use the hbtn_0c_0 database
-USE hbtn_0c_0;
-
--- Convert the character set and collation of the table first_table
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the character set and collation of the field name in the table first_table
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
