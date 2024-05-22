8e4ceb9a3e67b122f6e646dbc3ed20f5e08880c2
Ngayep <courtesyofjess@gmail.com>
2024-05-22 15:48:34 +0100

    readme, scripts 0 and 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This is a script that creates the MySQL server user user_0d_1
+
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT PRIVILEGE ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
