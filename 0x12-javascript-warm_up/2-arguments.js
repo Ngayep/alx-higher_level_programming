43ff537d504414b518be9aad80ba0c14081004e5
Ngayep <courtesyofjess@gmail.com>
2024-06-18 18:47:09 +0100

    2-arguments.js added

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+//a script that prints the first argument passed to it
+
+const numArgs = process.argv.length - 2;
+
+if (numArgs === 0) {
+	console.log('No argument');
+} else if (numArgs === 1) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
