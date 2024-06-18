0c91db37923fdb8d65e21a76869f9a70132fbb17
Ngayep <courtesyofjess@gmail.com>
2024-06-19 00:45:27 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,9 @@
 if (process.argv.lenght <= 3) {
   console.log(0);
 } else {
-  const arr = process.argv.map(Number);
-  const second = arr.sort(function (a, b) { return a - b; })[process.argv.length - 2];
-  console.log(second);
+  const arr = process.argv
+    .map(Number);
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
