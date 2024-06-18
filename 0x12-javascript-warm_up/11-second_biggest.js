1ff1690be3b05cffec61fb48dde605454af01832
Ngayep <courtesyofjess@gmail.com>
2024-06-19 00:50:06 +0100

    11-second_biggest.js fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,8 +4,8 @@ if (process.argv.lenght <= 3) {
   console.log(0);
 } else {
   const arr = process.argv
-    .map(Number);
-    .slice(2, process.argv.length)
+    .map(Number)
+    .slice(2, process.arr.length)
     .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
+  console.log(arr[arr.length - 2]);
 }
