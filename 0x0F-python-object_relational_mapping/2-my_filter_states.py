2740d5fe998347bd48bcb03680a4d33da547baf1
Ngayep <courtesyofjess@gmail.com>
2024-06-25 11:24:06 +0100

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,23 +5,48 @@ Lists all values in the states tables
 of a database where name
 matches the argument
 """
-import sys
 import MySQLdb
+from sys import argv, exit
+
+if len(argv) != 5:
+    print("Usage: ./2-my_filter_states.py <username> <password> "
+          "<database> <search>")
+    exit(1)
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(
-            user=sys.argv[1],
-            pwd=sys.argv[2],
-            db=sys.argv[3],
+    try:
+        # Connect to the database
+        db = MySQLdb.connect(
+            host='localhost',
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
             port=3306
-    )
+        )
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                   .format(sys.argv[4]))
+        # Create a cursor object
+        cursor = db.cursor()
 
-    rows = cur.fetchall()
-    for row in rows:
-        print(state)
-    cursor.close()
-    db.close()
+        # Prepare the SQL query
+        query = ("SELECT * "
+                 "FROM states "
+                 "WHERE name LIKE BINARY %s "
+                 "ORDER BY id ASC")
+
+        # Execute the query
+        cursor.execute(query, (argv[4],))
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Print the results
+        for row in rows:
+            print(row)
+
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+
+    except MySQLdb.OperationalError as e:
+        print(f"Error: {e}")
+        exit(1)
