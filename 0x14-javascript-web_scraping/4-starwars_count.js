9b0fa27304f9b9f84d632ed59bef601e04d5d808
Ngayep <courtesyofjess@gmail.com>
2024-08-03 13:22:44 +0100

    starwars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-const apiUrl = process.argv[2];
-const wedgeId = 18;
 
+// Get the API URL from command line arguments
+const apiUrl = process.argv[2];
+const characterId = 18; // Wedge Antilles character ID
+
+// Fetch data from the API
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
@@ -11,17 +14,18 @@ request(apiUrl, (error, response, body) => {
   }
 
   try {
-    const data = JSON.parse(body);
-    const films = data.results; // The results contain the list of movies
+    const films = JSON.parse(body).results;
     let count = 0;
 
+    // Iterate through each film
     films.forEach(film => {
-      if (film.characters.some(character => character.endsWith(wedgeId))) {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
         count++;
       }
     });
 
     console.log(count);
+
   } catch (parseError) {
     console.error('Error parsing JSON:', parseError);
   }
